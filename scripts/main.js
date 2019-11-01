5e545ef58702b3457bc93aee9a36811b28c1a489
VerloreN <37268930+maksBADcosmos@users.noreply.github.com>
2019-11-01 18:15:57 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 function comingsoon() {
-    alert('Coming soon...');
+    Swal.fire({
+        title: '<strong>Здесь ничего нет</strong>',
+        type: 'info',
+        html:
+          'Легенды гласят, что <b>однажды</b>, ' +
+          'при нажатии на кнопку откроется страница...',
+        showCloseButton: true,
+        showCancelButton: false,
+        focusConfirm: false,
+
+        confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> Ясно!',
+        confirmButtonAriaLabel: 'Ясно, понятно',
+      });
 }
